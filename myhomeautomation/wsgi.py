f5e2c71b064ccc7961e7c2b6d5779279d34c6380
Nathanael 100783830 <94251955+JS-Nate@users.noreply.github.com>
2023-11-22 14:21:51 -0500

    Update wsgi.py

--- myhomeautomation/wsgi.py
+++ myhomeautomation/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for myhomeautomation project.
+WSGI config for lightapp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myhomeautomation.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lightapp.settings")
 
 application = get_wsgi_application()
